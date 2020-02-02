4f0ec8e717fd561246a66d24caadedb70bb3ce4e
Sam Jantz <sjantz0@gmail.com>
2020-02-02 14:30:58 -0600

    functional mini game

--- game/main.js
+++ game/main.js
@@ -1,4 +1,9 @@
 'use strict';
+import Boot from './states/boot';
+import Play from './states/play';
+import Menu from './states/menu';
+import Preloader from './states/preload';
+import Gameover from './states/gameover';
 
 //global variables
 var BootState, GameoverState, MenuState, PlayState, PreloadState, game;
@@ -7,15 +12,15 @@ window.onload = function () {
 
   // Game States
   
-  game.state.add('boot', BootState);
+  game.state.add('boot', new Boot());
   
-  game.state.add('gameover', GameoverState);
+  game.state.add('gameover', new Gameover());
   
-  game.state.add('menu', MenuState);
+  game.state.add('menu', new Menu());
   
-  game.state.add('play', PlayState);
+  game.state.add('play', new Play());
   
-  game.state.add('preload', PreloadState);
+  game.state.add('preload', new Preloader());
   
 
   game.state.start('boot');
